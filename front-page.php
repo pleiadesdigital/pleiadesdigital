5e9d0a19b08486c3367c454620bcf313662b1cb6
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-08-25 20:31:34 -0400

    THU AUG25 20:31

--- front-page.php
+++ front-page.php
@@ -2,10 +2,53 @@
 
 
 <div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">
-    <h2>This is the FRONT PAGE!</h2>
+  <main id="main" class="site-main lander-page" role="main">
 
+    <section id="call-to-action">
+      <div class="indent">
+        <div class="front-left">
+          <h2 class="section-title">Call to Action</h2>
+        </div><!--class="front-left"-->
+        <div class="front-right">
+          Form
+        </div><!--class="front-right"-->
+      </div><!--class="indent"-->
+    </section><!--id="call-to-action"-->
 
+    <section id="testimonials">
+      <div class="indent">
+        <h2>Testimonials</h2>
+      </div><!--class="indent"-->
+    </section><!--id="testimonials"-->
+
+    <section id="services">
+      <div class="indent">
+        <h2 class="section-title">Services</h2>
+        some text goes here
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div><!--class="indent"-->
+    </section>
+
+    <section id="meet">
+      <div class="indent">
+        <h2 class="section-title">Meet the doctor</h2>
+      </div><!--class="indent"-->
+    </section><!--id="meet"-->
+
+    <section id="contact">
+      <div class="indent">
+        <div class="front-left">
+          <h2 class="section-title">Contact us</h2>
+        </div>
+        <div class="front-right">
+          Address
+        </div>
+      </div><!--class="indent"-->
+    </section>
 
 
 
